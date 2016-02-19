837e4651639b7600422cd1be7f3df50557837c44
Daniel Levy <daniellevy@Daniels-MacBook-Pro.local>
2016-02-19 12:12:11 -0500

    associated assignments with courses where assignments are deleting if course is deleted

---
+++ selfjoining.rb
@@ -0,0 +1,36 @@
+# an assignment has many pre-class lessons in this instance
+
+class Migration <ActiveRecord::Migration
+
+  def change
+    create_table :adults do |t|
+      t.string :name_array
+    end
+
+    create_table :children do |t|
+      t.string :name_array
+      t.integer :father_id
+      t.integer :mother_id
+    end
+  end
+end
+
+class Adult < ActiveRecord::Base
+  has_many :children_as_father, clas_name: "Child", foreign_key: "father_id"
+  has_many :children_as_mother, clas_name: "Child", foreign_key: "mother_id"
+
+  def children
+    children_as_father + children_as_mother
+  end
+end
+
+
+class Child < ActiveRecord::Base
+  belongs_to :father, class_name: "Adult", foreign_key: "father_id"
+  belongs_to :mother, class_name: "Adult", foreign_key: "mother_id"
+
+#bonus stuff
+def children
+  (children_as_father + children_as_mother)
+end
+end
--- tests.rb
+++ tests.rb
@@ -28,34 +28,57 @@ class ApplicationTest < Minitest::Test
   end
 
   def test_associate_schools_with_terms
-     school = School.create(name: "Haavad")
-     term = Term.create(name: "Fall")
-     school.terms << term
-     assert school.terms.include?(term)
-     assert_equal school, term.school
+    school = School.create(name: "Haavad")
+    term = Term.create(name: "Fall")
+    school.terms << term
+    assert school.terms.include?(term)
+    assert_equal school, term.school
 
   end
 
   def test_associate_terms_with_courses_not_deletable_if
-      term = Term.create(name: "Fall")
-      course = Course.create(name: "Biologie")
-      term.courses << course
-      term.destroy
-      refute term.destroyed?
+    term = Term.create(name: "Fall")
+    course = Course.create(name: "Biologie")
+    term.courses << course
+    assert term.courses.include?(course)
+    assert_equal term, course.term
+    term.destroy
+    refute term.destroyed?
   end
 
 
   def test_associate_course_with_course_students_not_deletable_if
-      course = Course.create(name: "Metalurgy")
-      course_student = CourseStudent.create(student_id: 100)
-      course.course_students <<  course_student
-      course.destroy
-      refute course.destroyed?
+    course = Course.create(name: "Metalurgy")
+    course_student = CourseStudent.create(student_id: 100)
+    course.course_students <<  course_student
+    assert course.course_students.include?(course_student)
+    assert_equal course, course_student.course
+    course.destroy
+    refute course.destroyed?
 
   end
 
-  def 
+  def test_associate_assignments_with_courses_deletable_if
+    course = Course.create(name: "Hermaphroditic_Postulations")
+    assignment = Assignment.create(name: "slug_obstetrics")
+    course.assignments << assignment
+    assert course.assignments.include?(assignment)
+    assert_equal assignment, course.assignments.last
+    course.destroy
+    assert course.destroyed?
 
+  end
 
+  #def test_associates_lessons_with_pre_class_assignments
+  #end
 
+  def test_set_school_to_have_many_courses_through_school_terms
+    school = School.create(name: "Haavad")
+    term = Term.create(name: "Fall")
+    c1 = Course.create(name: "Hermaphroditic_Postulations")
+    c2 = Course.create(name: "Phrenologystical metapharmacology2")
+    c3 = Course.create(name: "history of humor")
+    
+
+  end
 end
